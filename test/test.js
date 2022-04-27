2d1619741eee546607ffd1a707bfc89c1fcceb32
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-04-27 18:52:33 +0300

    test

---
+++ test/test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+(async () => {
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
+	await page.goto(`file:${path.join(__dirname, '../index.html')}`);
+	await page.waitForSelector('#inputLoadFiles');
+	const inputUploadHandle = await page.$('#inputLoadFiles');
+	const dicomDir = 'artifacts/DICOM/'
+	const dicomFileNameArray = fs.readdirSync(dicomDir);
+	const dicomFilePathArray = dicomFileNameArray.map(file => `${dicomDir}${file}`);
+	inputUploadHandle.uploadFile(...dicomFilePathArray);
+	await page.waitForSelector('#inputRangeFileIndex:not([disabled])');
+	await page.evaluate(() => {
+		document.querySelector('#inputRangeFileIndex').value = 42;
+		document.querySelector('#inputRangeFileIndex').oninput();
+		document.querySelector('#inputCheckboxShowEmptyOriginalTags').click();
+		document.querySelector('#inputCheckboxShowEmptyOriginalTags').click();
+	});
+	await page.waitForXPath('/html/body/table/tbody/tr[57]/td[4]').then(selector => selector.click())
+	await page.keyboard.type('20220101');
+	await page.waitForXPath('/html/body/table/tbody/tr[57]/td[5]').then(selector => selector.click())
+	await page.evaluate(() => {
+		document.querySelector('#inputRangeFileIndex').value = 150;
+		document.querySelector('#inputRangeFileIndex').oninput();
+		document.querySelector('#inputCheckboxShowEmptyOriginalTags').click();
+		document.querySelector('#inputCheckboxShowEmptyOriginalTags').click();
+	});
+	await page.screenshot({
+		path: 'artifacts/test.png',
+		fullpage: true
+	});
+	await page.close();
+	await browser.close();
+})();
