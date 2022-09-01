61843630ad228c1fe2384e1b882ae0e054fc043d
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-09-01 17:44:47 +0300

    further simplification

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const crypto = require('crypto');
+const dcmjs = require('dcmjs');
 const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
@@ -13,44 +14,187 @@ function waitFile(fileName) {
 }
 
 (async () => {
+	const jsonDataset = `{
+		"AccessionNumber": "",
+			"AcquisitionMatrix": [ 256, 0, 0, 192 ],
+			"AcquisitionNumber": "8",
+			"BitsAllocated": 16,
+			"BitsStored": 16,
+			"Columns": 256,
+			"ContentDate": "20020628",
+			"ContentTime": "164936.0",
+			"ContrastBolusAgent": "",
+			"DeviceSerialNumber": "4168496325",
+			"EchoTime": "6",
+			"EchoTrainLength": "1",
+			"FlipAngle": "3",
+			"FrameOfReferenceUID": "2.16.840.1.113662.4.4168496325.1025305873.7118351817185979330",
+			"HighBit": 15,
+			"ImageOrientationPatient": [ "0.00000e+00", "1.00000e+00", "-0.00000e+00", "-0.00000e+00", "0.00000e+00", "-1.00000e+00" ],
+			"ImagePositionPatient": [ "6.454490e+01", "-1.339286e+02", "1.167857e+02" ],
+			"ImageType": [ "ORIGINAL", "PRIMARY", "OTHER" ],
+			"ImagesInAcquisition": "130",
+			"ImagingFrequency": "6.37165e+01",
+			"InstanceNumber": "18",
+			"InstitutionName": "UCI Medical Center",
+			"MRAcquisitionType": "",
+			"MagneticFieldStrength": "1.5",
+			"Manufacturer": "Marconi Medical Systems, Inc.",
+			"ManufacturerModelName": "Eclipse 1.5T",
+			"Modality": "MR",
+			"NumberOfAverages": "1",
+			"NumberOfTemporalPositions": "1",
+			"OperatorsName": "SSRT/^^^^",
+			"PatientAge": "039Y",
+			"PatientBirthDate": "19000000",
+			"PatientID": "0000000",
+			"PatientName": "Zzzzzz^Yyyyy^^^",
+			"PatientPosition": "HFS",
+			"PatientSex": "M",
+			"PatientWeight": "99.773300",
+			"PercentPhaseFieldOfView": "91.4062",
+			"PhotometricInterpretation": "MONOCHROME2",
+			"PixelRepresentation": 1,
+			"PixelSpacing": [ "1.000000e+00", "1.000000e+00" ],
+			"PositionReferenceIndicator": "BRAIN",
+			"ProtocolName": "SAG/RF-FAST/VOL/FLIP 3",
+			"ReceiveCoilName": "HEAD",
+			"ReferencedImageSequence": {
+				"ReferencedSOPClassUID": "1.2.840.10008.5.1.4.1.1.4",
+				"ReferencedSOPInstanceUID": "2.16.840.1.113662.4.4168496325.1025306066.995213515550827262",
+				"_vrMap": {}
+			},
+			"ReferringPhysicianName": "",
+			"RepetitionTime": "20",
+			"Rows": 256,
+			"SOPClassUID": "1.2.840.10008.5.1.4.1.1.4",
+			"SOPInstanceUID": "2.16.840.1.113662.4.4168496325.1025307679.1207625440068523197",
+			"SamplesPerPixel": 1,
+			"ScanOptions": "",
+			"ScanningSequence": "GR",
+			"SequenceVariant": [
+				"SS",
+				"SP"
+			],
+			"SeriesDate": "20020628",
+			"SeriesDescription": "SAG/RF-FAST/VOL/FLIP 3",
+			"SeriesInstanceUID": "2.16.840.1.113662.4.4168496325.1025307678.3494705239865384161",
+			"SeriesNumber": "5",
+			"SeriesTime": "164118.0",
+			"SliceLocation": "64.544899",
+			"SliceThickness": "1.300000e+00",
+			"SoftwareVersions": "VIA2.0E.003",
+			"SpecificCharacterSet": "ISO_IR 100",
+			"StationName": "ba187_ws",
+			"StudyDate": "20020628",
+			"StudyDescription": "BRAIN",
+			"StudyID": "14024",
+			"StudyInstanceUID": "2.16.840.1.113662.4.4168496325.1025305873.7118351817185979330",
+			"StudyTime": "160956.0",
+			"TemporalPositionIdentifier": "1",
+			"TemporalResolution": "458660",
+			"WindowCenter": "28",
+			"WindowWidth": "20",
+			"_meta": {
+				"FileMetaInformationVersion": {
+					"Value": [
+						{
+							"0": 0,
+							"1": 1
+						}
+					],
+					"vr": "OB"
+				},
+				"ImplementationClassUID": {
+					"Value": [
+						"1.2.840.113819.7.1.1997.1.0"
+					],
+					"vr": "UI"
+				},
+				"ImplementationVersionName": {
+					"Value": [
+						"SENSORSYSTEMS1.0"
+					],
+					"vr": "SH"
+				},
+				"MediaStorageSOPClassUID": {
+					"Value": [
+						"1.2.840.10008.5.1.4.1.1.4"
+					],
+					"vr": "UI"
+				},
+				"MediaStorageSOPInstanceUID": {
+					"Value": [
+						"2.16.840.1.113662.4.4168496325.1025307679.1207625440068523197"
+					],
+					"vr": "UI"
+				},
+				"TransferSyntaxUID": {
+					"Value": [
+						"1.2.840.10008.1.2"
+					],
+					"vr": "UI"
+				}
+			},
+			"_vrMap": {
+				"PixelData": "OW"
+			}
+	}`;
+
 	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
 	const artifactsDir = process.env.ARTIFACTS_DIR;
 	await page._client().send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.resolve(artifactsDir)});
-	for (let i = 0; i < 9; i++) {
-		const inputDicomFileName = `N2D_000${i+1}.dcm`;
+	for (let i = 0; i < 10000; i++) {
+		let inputDicomFileName = `file-name-${i}.dcm`;
+		if (i % 7 != 0) {
+			const newPath = `file-path-${i % 7}`;
+			inputDicomFileName = `${newPath}/${inputDicomFileName}`;
+			if (!fs.existsSync(`${artifactsDir}/${newPath}`)){
+				fs.mkdirSync(`${artifactsDir}/${newPath}`);
+			}
+		}
+
 		if (!(fs.existsSync(`${artifactsDir}/${inputDicomFileName}`))) {
-			await page.goto(`https://github.com/datalad/example-dicom-structural/blob/master/dicoms/${inputDicomFileName}`);
-			await page.waitForSelector('#raw-url').then(selector => selector.click());
-			waitFile(`${artifactsDir}/${inputDicomFileName}`);
+			let dataset = JSON.parse(jsonDataset);
+			dataset.PatientName = `John Doe ${i % 100}`;
+			dataset.SeriesNumber = i % 5;
+			if (i % 3 === 0) {
+				dataset.Modality = 'CT';
+			} else if (i % 3 === 1) {
+				dataset.Modality = 'MG';
+			} else if (i % 3 === 2) {
+				dataset.Modality = 'MR';
+			}
+			let pixelData = new Int16Array(dataset.Rows * dataset.Columns);
+			pixelData.fill(100);
+			dataset.PixelData = pixelData.buffer;
+			const dicomDict = dcmjs.data.datasetToDict(dataset);
+			const buffer = Buffer.from(dicomDict.write());
+			fs.writeFileSync(`${artifactsDir}/${inputDicomFileName}`, buffer);
 		}
 	}
 	await page.goto(`file:${path.join(__dirname, 'index.html')}`);
 	await page.waitForSelector('#loadFilesInputFile:not([disabled])');
 	const inputUploadHandle = await page.$('#loadFilesInputFile');
-	let dicomFileNameArray = fs.readdirSync(artifactsDir).filter(fn => fn.endsWith('.dcm')).filter(fn => fn.startsWith('N2D_'));
+	let dicomFileNameArray = fs.readdirSync(artifactsDir).filter(fn => fn.endsWith('.dcm')).filter(fn => fn.startsWith('file-name-0'));
 	let dicomFilePathArray = dicomFileNameArray.map(file => `${artifactsDir}/${file}`);
 	inputUploadHandle.uploadFile(...dicomFilePathArray);
 	if (fs.existsSync(`${artifactsDir}/de-identified-files.zip`)) {
 		await fs.unlinkSync(`${artifactsDir}/de-identified-files.zip`);
 	}
-	await page.waitForSelector('#saveDeidentifiedFilesAsZipButton').then(selector => selector.click());
+	await page.waitForSelector('#saveProcessedFilesAsZipButton').then(selector => selector.click());
 	waitFile(`${artifactsDir}/de-identified-files.zip`);
 	const zipFileBuffer = new fs.readFileSync(`${artifactsDir}/de-identified-files.zip`);
 	const zipFileHash = crypto.createHash('sha256').update(zipFileBuffer).digest('hex');
-	assert.strictEqual(zipFileHash, 'a5ceb7b4ecb02f435fee19086ddc3ca1a184ebf9696fe7aae234375a15625d3c');
-	await page.waitForSelector('#saveDeidentifiedPatientNamesButton').then(selector => selector.click());
-	waitFile(`${artifactsDir}/de-identified-patient-names.csv`);
-	const deIdentifiedPatientNamesBuffer = new fs.readFileSync(`${artifactsDir}/de-identified-patient-names.csv`);
-	const deIdentifiedPatientNamesHash = crypto.createHash('sha256').update(deIdentifiedPatientNamesBuffer).digest('hex');
-	assert.strictEqual(deIdentifiedPatientNamesHash, '26c93101ae6f2ab670a97f1408b10933bb3e5867a9186d712dc8910271e85ac9');
+	assert.strictEqual(zipFileHash, 'd8e57a31420975a8da8d9e69741f56d4cec693d6dbc95c26b7190047189390f2');
 	await page.screenshot({
 		path: `${artifactsDir}/puppeteer-screenshot.png`
 	});
 	const screenshotBuffer = new fs.readFileSync(`${artifactsDir}/puppeteer-screenshot.png`);
 	const screenshotHash = crypto.createHash('sha256').update(screenshotBuffer).digest('hex');
 	if (process.env.GITHUB_ACTIONS === undefined) {
-		assert.strictEqual(screenshotHash, '675619f23182e41fa450aee75375e124766c273a77d4ee34577e111b438cc470');
+		assert.strictEqual(screenshotHash, '54e8170213ba9dfddb49b0f5cb4c420d9ba85121e1da292a8def400da3394fb6');
 	}
 	await page.close();
 	await browser.close();
--- script.js
+++ script.js
@@ -3,31 +3,46 @@
 const dateProcessingSelect = document.getElementById('dateProcessingSelect');
 const dicomTagArray = Object.keys(dcmjs.data.DicomMetaDictionary.nameMap);
 const dicomTagSavePathInputText = document.getElementById('dicomTagSavePathInputText');
+const filesProcessedSpan = document.getElementById('filesProcessedSpan');
 const nemaTable = JSON.parse(nemaTableString);
 const retainUidsInputCheckbox = document.getElementById('retainUidsInputCheckbox');
-const saveDeidentifiedFilesAsZipButton = document.getElementById('saveDeidentifiedFilesAsZipButton');
-const saveDeidentifiedPatientNamesButton = document.getElementById('saveDeidentifiedPatientNamesButton');
+const saveProcessedFilesAsZipButton = document.getElementById('saveProcessedFilesAsZipButton');
 let dicomDictArray = [];
 let fileArray = [];
 let filesNum = 0;
-let patientNameArray = [];
+let patientNameObject = {};
 
 function disableUI(argument) {
 	dateProcessingSelect.disabled = argument;
-	dicomTagSavePathInputText.disabled = argument;
 	retainUidsInputCheckbox.disabled = argument;
-	saveDeidentifiedFilesAsZipButton.disabled = argument;
-	saveDeidentifiedPatientNamesButton.disabled = argument;
+	saveProcessedFilesAsZipButton.disabled = argument;
 }
 
-function hashCode(str) {
-	let hash = 0;
-	for (let i = 0, len = str.length; i < len; i++) {
-		let chr = str.charCodeAt(i);
-		hash = (hash << 5) - hash + chr;
-		hash |= 0;
+function hashCode(string) {
+	const utf8 = new TextEncoder().encode(string);
+	return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+		const hashArray = Array.from(new Uint8Array(hashBuffer));
+		const hashHex = hashArray
+			.map((bytes) => bytes.toString(16).padStart(2, '0'))
+			.join('');
+		return hashHex;
+	});
+}
+
+function onloadDeidentiedPatientNamesInputFile() {
+	const file = event.currentTarget.files[0];
+	if (file.length === 0) {
+		return;
 	}
-	return hash;
+	const reader = new FileReader();
+	reader.onload = function (e) {
+		const text = e.target.result;
+		const rowArray = text.split('\n');
+		for (let i = 0; i < rowArray.length; i++) {
+			patientNameObject[rowArray[i][0]] = rowArray[i][1];
+		}
+	};
+	reader.readAsText(file);
 }
 
 function onloadFilesOrDirectory() {
@@ -51,112 +66,75 @@ function saveData(blob, fileName) {
 	window.URL.revokeObjectURL(url);
 }
 
-dicomTagSavePathInputText.onkeydown = function() {
-	dicomTagSavePathInputText.setCustomValidity('');
-}
-
-dicomTagSavePathInputText.oninput = function() {
-	const cursorPosition = dicomTagSavePathInputText.selectionStart - 1;
-	const hasInvalidCharacters = dicomTagSavePathInputText.value.match(/[^0-9^A-E/]/);
-	if (!hasInvalidCharacters) {
-		return;
-	}
-	dicomTagSavePathInputText.value = dicomTagSavePathInputText.value.replace(/[^0-9^A-E/]/g, '');
-	dicomTagSavePathInputText.setSelectionRange(cursorPosition, cursorPosition);
-}
-
-function readDeidentifyAndAddToZipFile(i, zip, regexpDate, numberOfDaysToAdd) {
-	const reader = new FileReader();
-	reader.onload = function() {
-		dicomDictArray[i] = dcmjs.data.DicomMessage.readFile(reader.result);
-		for (const property in nemaTable) {
-			if (dicomDictArray[i].dict[property]) {
-				if (nemaTable[property][3] === 'C') {
-					let originalDate = dicomDictArray[i].dict[property].Value[0];
-					if (originalDate !== '') {
-						if (dateProcessingSelect.value === 'dateProcessingRandomShiftOption') {
-							const validDate = regexpDate.test(originalDate);
-							if (validDate) {
-								originalDate = originalDate.replace(regexpDate, "$1-$2-$3");
-								originalDate = new Date(originalDate);
-								let unixDate = originalDate.setDate(originalDate.getDate() + numberOfDaysToAdd);
-								let alteredDate = new Date(unixDate);
-								let formattedDate = alteredDate.getUTCFullYear().toString() + (alteredDate.getMonth() + 1).toLocaleString('en-US', {minimumIntegerDigits: 2}) + (alteredDate.getDay() + 1).toLocaleString('en-US', {minimumIntegerDigits: 2});
-								dicomDictArray[i].dict[property].Value = [formattedDate];
-							}
-							continue;
-						}
-						if (dateProcessingSelect.value === 'dateProcessingEmptyOption') {
-							dicomDictArray[i].dict[property].Value = [];
-						}
-					}
-					continue;
-				}
-				if (nemaTable[property][1] === 'K' && retainUidsInputCheckbox.checked) {
-					continue;
-				}
-				dicomDictArray[i].dict[property].Value = [];
-			}
-		}
-		const patientName = dicomDictArray[i].dict['00100010'].Value[0];
-		if (!patientNameArray.includes(patientName)) {
-			patientNameArray.push(patientName);
-		}
-		dicomDictArray[i].dict['00100010'].Value[0] = `Patient-${patientNameArray.indexOf(patientName)}`;
-		let dicomTagValueSavePath = '';
-		if (dicomTagSavePathInputText.value !== '') {
-			const dicomTagSavePathArray = dicomTagSavePathInputText.value.slice(0, -1).split('/');
-			for (const dicomTagSavePath of dicomTagSavePathArray) {
-				if (!(dicomDictArray[i].dict[dicomTagSavePath])) {
-					dicomTagSavePathInputText.setCustomValidity('invalid');
-					return;
-				} else if (!(dicomDictArray[i].dict[dicomTagSavePath].Value)){
-					dicomTagSavePathInputText.setCustomValidity('invalid');
-					return;
-				} else if (!(dicomDictArray[i].dict[dicomTagSavePath].Value[0])){
-					dicomTagSavePathInputText.setCustomValidity('invalid');
-					return;
-				}
-			}
-			dicomTagValueSavePath = dicomTagSavePathArray.map(x => dicomDictArray[i].dict[x].Value[0]).join('/') + '/';
-		}
-		const dicomTagValueSavePathArray = dicomTagValueSavePath.split('/');
-		for (let j = dicomTagValueSavePathArray.length; j > 0; j--) {
-			zip.file(dicomTagValueSavePathArray.slice(0, j).join('/') + '/', '', {date: new Date("January 02, 2000 00:00:00")});
-		}
-		let dateNow;
-		if (navigator.webdriver) {
-			dateNow = i;
-		} else {
-			dateNow = Date.now();
-		}
-		const fileName = `${dateNow}-${hashCode(JSON.stringify(dicomDictArray[i]))}`;
-		zip.file(`${dicomTagValueSavePath}${fileName}.dcm`, dicomDictArray[i].write({allowInvalidVRLength: true}), {date: new Date("January 02, 2000 00:00:00")});
-		if (i === filesNum - 1) {
-			zip.generateAsync({type:'blob'})
-				.then(function (blob) {
-					saveData(blob, 'de-identified-files.zip');
-				});
-		}
-	}
-	reader.readAsArrayBuffer(fileArray[i]);
-}
-
-saveDeidentifiedFilesAsZipButton.onclick = function() {
+saveProcessedFilesAsZipButton.onclick = function() {
 	disableUI(true);
 	const numberOfDaysToAdd = Math.floor((Math.random() - 0.5) * 200);
 	const regexpDate = /(\d{4})(\d{2})(\d{2})/;
 	const zip = new JSZip();
 	for (let i = 0; i < filesNum; i++) {
-		readDeidentifyAndAddToZipFile(i, zip, regexpDate, numberOfDaysToAdd);
+		const reader = new FileReader();
+		reader.onload = function() {
+			dicomDictArray[i] = dcmjs.data.DicomMessage.readFile(reader.result);
+			for (const property in nemaTable) {
+				if (dicomDictArray[i].dict[property]) {
+					if (nemaTable[property][3] === 'C') {
+						let originalDate = dicomDictArray[i].dict[property].Value[0];
+						if (originalDate !== '') {
+							if (dateProcessingSelect.value === 'dateProcessingRandomShiftOption') {
+								const validDate = regexpDate.test(originalDate);
+								if (validDate) {
+									originalDate = originalDate.replace(regexpDate, "$1-$2-$3");
+									originalDate = new Date(originalDate);
+									let unixDate = originalDate.setDate(originalDate.getDate() + numberOfDaysToAdd);
+									let alteredDate = new Date(unixDate);
+									let formattedDate = alteredDate.getUTCFullYear().toString() + (alteredDate.getMonth() + 1).toLocaleString('en-US', {minimumIntegerDigits: 2}) + (alteredDate.getDay() + 1).toLocaleString('en-US', {minimumIntegerDigits: 2});
+									dicomDictArray[i].dict[property].Value = [formattedDate];
+								}
+								continue;
+							}
+							if (dateProcessingSelect.value === 'dateProcessingEmptyOption') {
+								dicomDictArray[i].dict[property].Value = [];
+							}
+						}
+						continue;
+					}
+					if (nemaTable[property][1] === 'K' && retainUidsInputCheckbox.checked) {
+						continue;
+					}
+					if (property === '00100010') {
+						const patientName = dicomDictArray[i].dict['00100010'].Value[0];
+						if (!(patientName in patientNameObject)) {
+							patientNameObject[patientName] = `Patient-${Object.keys(patientNameObject).length}`;
+						}
+						dicomDictArray[i].dict['00100010'].Value[0] = patientNameObject[patientName];
+					} else {
+						dicomDictArray[i].dict[property].Value = [];
+					}
+				}
+				filesProcessedSpan.textContent = `${i+1}/${filesNum}`;
+			}
+			hashCode(JSON.stringify(dicomDictArray[i])).then((hex) => {
+				const dicomTagSavePathArray = dicomTagSavePathInputText.value.slice(0, -1).split('/');
+				const dicomTagValueSavePath = dicomTagSavePathArray.map(x => dicomDictArray[i].dict[x].Value[0]).join('/') + '/';
+				const dicomTagValueSavePathArray = dicomTagValueSavePath.split('/');
+				for (let j = 1; j < dicomTagValueSavePathArray.length; j++) {
+					zip.file(dicomTagValueSavePathArray.slice(0, j).join('/') + '/', '', {date: new Date("January 02, 2000 00:00:00")});
+				}
+				zip.file(`${dicomTagValueSavePath}${hex}.dcm`, dicomDictArray[i].write({allowInvalidVRLength: true}), {date: new Date("January 02, 2000 00:00:00")});
+				if (i === filesNum - 1) {
+					const csvContent = Object.entries(patientNameObject).map((patientName, index) => patientName).join('\n') + '\n';
+					const blob = new Blob([csvContent]);
+					zip.file('de-identified-patient-names.csv', blob, {date: new Date("January 02, 2000 00:00:00")});
+					zip.generateAsync({type:'blob'})
+						.then(function (blob) {
+							saveData(blob, 'de-identified-files.zip');
+							disableUI(false);
+						});
+				}
+			})
+		}
+		reader.readAsArrayBuffer(fileArray[i]);
 	}
-	disableUI(false);
-}
-
-saveDeidentifiedPatientNamesButton.onclick = function() {
-	const csvContent = 'Patient name, Patient name (De-identified)\n' + patientNameArray.map((patientName, index) => `${patientName}, Patient-${index}`).join('\n') + '\n';
-	const blob = new Blob([csvContent]);
-	saveData(blob, 'de-identified-patient-names.csv');
 }
 
 disableUI(true);
