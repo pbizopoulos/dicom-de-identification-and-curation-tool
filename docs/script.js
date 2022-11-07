e6452ee0daabfd4eb5b4b7af67da347c0d2ab654
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-11-08 01:40:34 +0200

    fix

--- docs/script.js
+++ docs/script.js
@@ -63,7 +63,6 @@ function onloadFilesOrDirectory() {
 function saveData(blob, fileName) {
 	const a = document.createElement('a');
 	document.body.appendChild(a);
-	a.style = 'display: none';
 	const url = window.URL.createObjectURL(blob);
 	a.href = url;
 	a.download = fileName;
