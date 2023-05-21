6d6fcc9f173450a457a0d45c9a623ee1774566e8
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-05-21 16:47:11 +0300

    .

--- docs/python/main.py
+++ docs/python/main.py
@@ -11,8 +11,10 @@ def main() -> None:
     bin_file_path = Path("bin")
     if not bin_file_path.exists():
         bin_file_path.mkdir(parents=True)
-    with request.urlopen("https://dicom.nema.org/medical/dicom/current/output/chtml/part15/chapter_e.html") as response: # noqa: S310
-       html = response.read()
+    with request.urlopen(  # noqa: S310
+        "https://dicom.nema.org/medical/dicom/current/output/chtml/part15/chapter_e.html",
+    ) as response:
+        html = response.read()
     df_tables = pd.read_html(html)
     df_table = df_tables[3]
     df_table = df_table.fillna("")
@@ -21,23 +23,47 @@ def main() -> None:
     retain_safe_private_option = df_table["Rtn. Safe Priv. Opt."].to_list()
     retain_uids_option = df_table["Rtn. UIDs Opt."].to_list()
     retain_device_identity_option = df_table["Rtn. Dev. Id. Opt."].to_list()
-    retain_patient_characteristics_option = ["K" if i == "C" else i for i in df_table["Rtn. Pat. Chars. Opt."].to_list()]
+    retain_patient_characteristics_option = [
+        "K" if i == "C" else i for i in df_table["Rtn. Pat. Chars. Opt."].to_list()
+    ]
     retain_long_modified_dates_option = df_table["Rtn. Long. Modif. Dates Opt."]
-    retain_description_option = ["K" if i == "C" else i for i in df_table["Clean Desc. Opt."].to_list()]
-    output = [basic_profile, retain_safe_private_option, retain_uids_option, retain_device_identity_option, retain_patient_characteristics_option, retain_long_modified_dates_option, retain_description_option]
-    output = list(map(list, zip(*output))) # noqa: B905
-    dicom_tag_to_nema_action = dict(zip(dicom_tags, output)) # noqa: B905
+    retain_description_option = [
+        "K" if i == "C" else i for i in df_table["Clean Desc. Opt."].to_list()
+    ]
+    output = [
+        basic_profile,
+        retain_safe_private_option,
+        retain_uids_option,
+        retain_device_identity_option,
+        retain_patient_characteristics_option,
+        retain_long_modified_dates_option,
+        retain_description_option,
+    ]
+    output = list(map(list, zip(*output)))  # noqa: B905
+    dicom_tag_to_nema_action = dict(zip(dicom_tags, output))  # noqa: B905
     dicom_tag_to_nema_action["00100010"][0] = "Z"
     dicom_tag_to_nema_action["00100020"][0] = "Z"
-    file_content = f"const dicomTagToNemaActionObject = {json.dumps(dicom_tag_to_nema_action)};"
+    file_content = (
+        f"const dicomTagToNemaActionObject = {json.dumps(dicom_tag_to_nema_action)};"
+    )
     with Path("dist/dicom-tag-to-nema-action.js").open("w", encoding="utf-8") as file:
         file.write(file_content)
-    assert sha256(file_content.encode("utf-8")).hexdigest() == "54dfa7ecb18b80983e18f2b3a629b588eb0c9e6f26e6c4b56026321ac4d5baaf"
-    with Path("bin/dicom-tag-to-nema-action-default.csv").open("w", encoding="utf-8", newline="") as file:
+    assert (
+        sha256(file_content.encode("utf-8")).hexdigest()
+        == "54dfa7ecb18b80983e18f2b3a629b588eb0c9e6f26e6c4b56026321ac4d5baaf"
+    )
+    with Path("bin/dicom-tag-to-nema-action-default.csv").open(
+        "w",
+        encoding="utf-8",
+        newline="",
+    ) as file:
         writer = csv.writer(file)
         writer.writerow(["Name", "Tag", "Action"])
         dicom_tag_names = df_table["Attribute Name"].to_list()
-        for (key, _), dicom_tag_name in zip(dicom_tag_to_nema_action.items(), dicom_tag_names): # noqa: B905
+        for (key, _), dicom_tag_name in zip(  # noqa: B905
+            dicom_tag_to_nema_action.items(),
+            dicom_tag_names,
+        ):
             if key in ["00100010", "00100020"]:
                 writer.writerow([dicom_tag_name.replace("\n", " "), key, "Z"])
             elif "C" in dicom_tag_to_nema_action[key]:
--- python/main.py
+++ python/main.py
@@ -13,7 +13,8 @@ class TestWebApplication(unittest.TestCase):
         bin_file_path = Path("bin")
         if not bin_file_path.exists():
             bin_file_path.mkdir(parents=True)
-        dicom_data = dcmread(pydicom.data.get_testdata_file("rtdose_1frame.dcm")) # type: ignore[arg-type]
+        testdata_file = pydicom.data.get_testdata_file("rtdose_1frame.dcm")
+        dicom_data = dcmread(testdata_file)  # type: ignore[arg-type]
         generated_data_file_path = Path("bin/generated-data")
         if not generated_data_file_path.exists():
             generated_data_file_path.mkdir(exist_ok=True)
@@ -21,16 +22,21 @@ class TestWebApplication(unittest.TestCase):
                 dicom_data[16, 16].value = f"John Doe {index % 100}"
                 dicom_data[16, 32].value = str(index % 100)
                 dicom_data[32, 17].value = str(index % 5)
-                dicom_data[8, 24].value = dicom_data[8, 24].value.replace(str(index % 8), str(index % 9))
+                dicom_data[8, 24].value = dicom_data[8, 24].value.replace(
+                    str(index % 8),
+                    str(index % 9),
+                )
                 if index % 3:
                     dicom_data[8, 96].value = "CT"
                 elif index % 3 == 1:
                     dicom_data[8, 96].value = "MG"
-                elif index % 3 == 2: # noqa: PLR2004
+                elif index % 3 == 2:  # noqa: PLR2004
                     dicom_data[8, 96].value = "MR"
                 if index % 7:
                     (generated_data_file_path / f"folder-{index}").mkdir(exist_ok=True)
-                    dicom_data.save_as(generated_data_file_path / f"folder-{index}" / f"{index}.dcm")
+                    dicom_data.save_as(
+                        generated_data_file_path / f"folder-{index}" / f"{index}.dcm",
+                    )
                 else:
                     dicom_data.save_as(generated_data_file_path / f"{index}.dcm")
         self.generated_data_file_path = generated_data_file_path
@@ -41,18 +47,33 @@ class TestWebApplication(unittest.TestCase):
             context = browser.new_context(record_video_dir="bin/")
             page = context.new_page()
             page.on("pageerror", self.page_error)
-            page.goto("https://dicom-de-identification-and-curation-tool.incisive.iti.gr/")
-            only_files_generated_data_file_path = [self.generated_data_file_path / file for file in os.listdir(self.generated_data_file_path) if (self.generated_data_file_path / file).is_file()]
-            page.set_input_files("#load-directory-input-file", sorted(only_files_generated_data_file_path))
+            page.goto(
+                "https://dicom-de-identification-and-curation-tool.incisive.iti.gr/",
+            )
+            only_files_generated_data_file_path = [
+                self.generated_data_file_path / file
+                for file in os.listdir(self.generated_data_file_path)
+                if (self.generated_data_file_path / file).is_file()
+            ]
+            page.set_input_files(
+                "#load-directory-input-file",
+                sorted(only_files_generated_data_file_path),
+            )
             with page.expect_download() as download_info:
                 page.click("#save-processed-files-as-zip-button")
             download = download_info.value
             download.save_as("bin/de-identified-files.zip")
             with Path("bin/de-identified-files.zip").open("rb") as file:
-                assert hashlib.sha256(file.read()).hexdigest() == "6ce13d6f3754db683e9130cf7498553d80a4a6ec935143793289939f55a4645e"
+                assert (
+                    hashlib.sha256(file.read()).hexdigest()
+                    == "6ce13d6f3754db683e9130cf7498553d80a4a6ec935143793289939f55a4645e"  # noqa: E501
+                )
             page.screenshot(path="bin/screenshot.png")
             with Path("bin/screenshot.png").open("rb") as file:
-                assert hashlib.sha256(file.read()).hexdigest() == "6a2288402832ba59d081e824ce476b018f8782f89612d0797e1d425011514be7"
+                assert (
+                    hashlib.sha256(file.read()).hexdigest()
+                    == "6a2288402832ba59d081e824ce476b018f8782f89612d0797e1d425011514be7"  # noqa: E501
+                )
             context.close()
             browser.close()
 
