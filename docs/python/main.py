8a3a56269cad1a795fd5dc8be35f8c956ce07e18
Paschalis Bizopoulos <pbizop@gmail.com>
2024-02-22 07:56:54 +0200

    .

---
+++ docs/python/main.py
@@ -0,0 +1,73 @@
+import csv
+import json
+from hashlib import sha256
+from pathlib import Path
+from urllib import request
+
+import pandas as pd
+
+
+def main() -> None:
+    with request.urlopen(
+        "https://dicom.nema.org/medical/dicom/current/output/chtml/part15/chapter_e.html",
+    ) as response:
+        html = response.read()
+    df_tables = pd.read_html(html)
+    df_table = df_tables[3]
+    df_table = df_table.fillna("")
+    dicom_tags = df_table["Tag"].str.replace("[(,)]", "", regex=True).to_list()
+    basic_profile = ["X" for i in df_table["Basic Prof."].to_list()]
+    retain_safe_private_option = df_table["Rtn. Safe Priv. Opt."].to_list()
+    retain_uids_option = df_table["Rtn. UIDs Opt."].to_list()
+    retain_device_identity_option = df_table["Rtn. Dev. Id. Opt."].to_list()
+    retain_patient_characteristics_option = [
+        "K" if i == "C" else i for i in df_table["Rtn. Pat. Chars. Opt."].to_list()
+    ]
+    retain_long_modified_dates_option = df_table["Rtn. Long. Modif. Dates Opt."]
+    retain_description_option = [
+        "K" if i == "C" else i for i in df_table["Clean Desc. Opt."].to_list()
+    ]
+    output = [
+        basic_profile,
+        retain_safe_private_option,
+        retain_uids_option,
+        retain_device_identity_option,
+        retain_patient_characteristics_option,
+        retain_long_modified_dates_option,
+        retain_description_option,
+    ]
+    output = list(map(list, zip(*output)))
+    dicom_tag_to_nema_action = dict(zip(dicom_tags, output))
+    dicom_tag_to_nema_action["00100010"][0] = "Z"
+    dicom_tag_to_nema_action["00100020"][0] = "Z"
+    file_content = (
+        f"const dicomTagToNemaActionObject = {json.dumps(dicom_tag_to_nema_action)};"
+    )
+    with Path("prm/dicom-tag-to-nema-action.js").open("w", encoding="utf-8") as file:
+        file.write(file_content)
+    assert (
+        sha256(file_content.encode("utf-8")).hexdigest()
+        == "2ba2580f38a162efc20e3e75f6c5b0defdfed6db091b5343804d712cae499df5"
+    )
+    with Path("tmp/dicom-tag-to-nema-action-default.csv").open(
+        "w",
+        encoding="utf-8",
+        newline="",
+    ) as file:
+        writer = csv.writer(file)
+        writer.writerow(["Name", "Tag", "Action"])
+        dicom_tag_names = df_table["Attribute Name"].to_list()
+        for (key, _), dicom_tag_name in zip(
+            dicom_tag_to_nema_action.items(),
+            dicom_tag_names,
+        ):
+            if key in ["00100010", "00100020"]:
+                writer.writerow([dicom_tag_name.replace("\n", " "), key, "Z"])
+            elif "C" in dicom_tag_to_nema_action[key]:
+                writer.writerow([dicom_tag_name.replace("\n", " "), key, "C"])
+            elif "K" not in dicom_tag_to_nema_action[key]:
+                writer.writerow([dicom_tag_name.replace("\n", " "), key, "X"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/main.py
@@ -0,0 +1,83 @@
+import hashlib
+import os
+import unittest
+from pathlib import Path
+
+import pydicom
+from playwright.sync_api import Error, sync_playwright
+from pydicom import dcmread
+
+
+class TestWebApplication(unittest.TestCase):
+    def setUp(self: "TestWebApplication") -> None:
+        testdata_file = pydicom.data.get_testdata_file("rtdose_1frame.dcm")
+        dicom_data = dcmread(testdata_file)  # type: ignore[arg-type]
+        generated_data_file_path = Path("tmp/generated-data")
+        if not generated_data_file_path.exists():
+            generated_data_file_path.mkdir(exist_ok=True)
+            for index in range(1000):
+                dicom_data[16, 16].value = f"John Doe {index % 100}"
+                dicom_data[16, 32].value = str(index % 100)
+                dicom_data[32, 17].value = str(index % 5)
+                dicom_data[8, 24].value = dicom_data[8, 24].value.replace(
+                    str(index % 8),
+                    str(index % 9),
+                )
+                if index % 3:
+                    dicom_data[8, 96].value = "CT"
+                elif index % 3 == 1:
+                    dicom_data[8, 96].value = "MG"
+                elif index % 3 == 2:  # noqa: PLR2004
+                    dicom_data[8, 96].value = "MR"
+                if index % 7:
+                    (generated_data_file_path / f"folder-{index}").mkdir(exist_ok=True)
+                    dicom_data.save_as(
+                        generated_data_file_path / f"folder-{index}" / f"{index}.dcm",
+                    )
+                else:
+                    dicom_data.save_as(generated_data_file_path / f"{index}.dcm")
+        self.only_files_generated_data_file_path = list(
+            {
+                generated_data_file_path / file
+                for file in os.listdir(generated_data_file_path)
+                if (generated_data_file_path / file).is_file()
+            },
+        )
+
+    def test_web_application(self: "TestWebApplication") -> None:
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(args=["--user-agent=playwright"])
+            context = browser.new_context(record_video_dir="tmp/")
+            page = context.new_page()
+            page.on("pageerror", self.page_error)
+            page.goto(
+                "https://dicom-de-identification-and-curation-tool.incisive.iti.gr/",
+            )
+            page.set_input_files(
+                "#load-directory-input-file",
+                self.only_files_generated_data_file_path,
+            )
+            with page.expect_download() as download_info:
+                page.click("#save-processed-files-as-zip-button")
+            download = download_info.value
+            download.save_as("tmp/de-identified-files.zip")
+            with Path("tmp/de-identified-files.zip").open("rb") as file:
+                assert (
+                    hashlib.sha256(file.read()).hexdigest()
+                    == "bf40ebb18b6e4767dad94c074d301b42132ebe02004bfbc7792ff3ed4bbc5642"  # noqa: E501
+                )
+            page.screenshot(path="tmp/screenshot.png")
+            with Path("tmp/screenshot.png").open("rb") as file:
+                assert (
+                    hashlib.sha256(file.read()).hexdigest()
+                    == "07963e07e9aad3ff14ed38abbec17c4cba7df56db26938e63445a2fe997276ba"  # noqa: E501
+                )
+            context.close()
+            browser.close()
+
+    def page_error(self: "TestWebApplication", exception: Error) -> None:
+        raise exception
+
+
+if __name__ == "__main__":
+    unittest.main()
